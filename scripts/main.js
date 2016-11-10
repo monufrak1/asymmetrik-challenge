e5b31f98e4fc9be675adaf2fec33ad2641fc37c1
matt.pc <monufrak1@gmail.com>
2016-11-09 23:50:58 -0500

    Add initial webgl rendering of quad with basic shaders

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,8 @@ var drawPanel;
 var canvasWidth = 800;
 var canvasHeight = 600;
 
+var particleScalar = 0.05;
+
 var particleSize;
 var particleType;
 
@@ -51,7 +53,6 @@ function main() {
     sizeSlider.oninput = function() {
         setParticleSize(sizeSlider.value);
     }
-
     
     playButton.disabled = true;
     pauseButton.disabled = false;
@@ -66,7 +67,7 @@ function toggleControlButtons() {
 }
 
 function setParticleSize(size) {
-    particleSize = size;
+    particleSize = size * particleScalar;
     sizeText.innerText = size;
 }
 
@@ -75,20 +76,114 @@ function setParticleType(type) {
 }
 
 function initWebgl() {
+    drawPanel.innerHTML = "";
+
     var canvas = document.createElement("canvas");
     canvas.id = "drawCanvas";
     canvas.setAttribute("width", canvasWidth + "px");
-    canvas.setAttribute("height", canvasWidth + "px");
+    canvas.setAttribute("height", canvasHeight + "px");
 
     drawPanel.appendChild(canvas);
 
     var gl = canvas.getContext("webgl");
     
     if(gl) {
-        gl.viewport(0, 0, canvas.width, canvas.height);
+        var particleSpeed = 0;
+
+        var vertices = [+0.5, -0.5, 0,
+                        +0.5, +0.5, 0,
+                        -0.5, -0.5, 0,
+                        -0.5, +0.5, 0];
+
+        // Create vertex buffer
+        var vertexBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW); 
+        vertexBuffer.itemSize = 3;
+        vertexBuffer.numItems = 4;
         
-        gl.clearColor(0, 0, 1.0, 1.0);
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        var vertexShaderSource =
+            'attribute vec3 a_position;' +
+            'uniform float u_scale;' +
+            'uniform mat4 u_translation;' +
+            'void main() {' +
+            '   vec3 pos = a_position;' +
+            '   pos *= u_scale;' +
+            '   gl_Position = u_translation * vec4(pos, 1);' +
+            '}';
+        var fragmentShaderSource =
+            'precision mediump float;' +
+            'void main() {' +
+            '   gl_FragColor = vec4(1.0, 0.0, 0.0, 1);' +
+            '}';
+        
+        function buildTranslationMatrix(x, y) {
+            return [1,0,0,0,
+                    0,1,0,0,
+                    0,0,1,0,
+                    x,y,0,1];  
+        }
+        
+        var buildShader = function (shaderSource, shaderType) {
+            var shader = gl.createShader(shaderType);
+            gl.shaderSource(shader, shaderSource);
+            gl.compileShader(shader);
+            if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+                alert (gl.getShaderInfoLog(shader));
+                gl.deleteShader(shader);
+            }
+            return shader;
+        }
+        
+        // Compile shaders
+        var compiledVertexShader = buildShader(vertexShaderSource, gl.VERTEX_SHADER);
+        var compiledFragmentShader = buildShader(fragmentShaderSource, gl.FRAGMENT_SHADER);
+        
+        // Create GLSL program
+        var program = gl.createProgram();
+        gl.attachShader(program,compiledVertexShader);
+        gl.attachShader(program,compiledFragmentShader);
+        gl.linkProgram(program);
+        gl.useProgram(program);
+        
+        // Get references to shader variables
+        var positionVarLoc = gl.getAttribLocation(program, "a_position");
+        var scaleVarLoc = gl.getUniformLocation(program, "u_scale");
+        var translationVarLoc = gl.getUniformLocation(program, "u_translation");
+
+        // Enable shaders
+        gl.enableVertexAttribArray(positionVarLoc);
+        gl.uniform1f(scaleVarLoc, particleSize);
+
+        // Set viewport and clear color
+        gl.viewport(0, 0, canvasWidth, canvasHeight);
+        gl.clearColor(0, 0, 0, 1.0);
+
+        var deltaTime = 0;
+
+        // Main render 'loop' function
+        function draw () {
+            deltaTime += 0.005;
+
+            // Clear back buffer
+            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+            var posX = 0;
+            var posY = 0;
+
+            // Create translation matrix
+            var transMatrix = buildTranslationMatrix(posX + deltaTime*particleSpeed, posY - deltaTime*particleSpeed);
+            
+            gl.uniformMatrix4fv(translationVarLoc, false, new Float32Array(transMatrix));
+            gl.vertexAttribPointer(positionVarLoc, vertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
+            
+            gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertexBuffer.numItems);
+        
+            window.requestAnimationFrame(draw);
+        }
+        
+        // Kick off render loop
+        draw();
     } else {
         alert("Webgl not supported. Imagine pretty particles...");
     }
